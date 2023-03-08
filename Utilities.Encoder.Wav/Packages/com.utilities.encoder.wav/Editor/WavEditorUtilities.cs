b3e1e86e184a855f6e93575c7bb391c75f6a3dd4
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-07 22:08:16 -0500

    com.utilities.encoder.wav 1.0.0-preview.1 (#1)

---
+++ Utilities.Encoder.Wav/Packages/com.utilities.encoder.wav/Editor/WavEditorUtilities.cs
@@ -0,0 +1,54 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.IO;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utilities.Encoding.Wav.Editor
+{
+    public class WavEditorUtilities
+    {
+        [MenuItem("CONTEXT/AudioClip/Convert to wav...", true)]
+        public static bool ConvertToWavValidate(MenuCommand menuCommand)
+        {
+            if (menuCommand.context is AudioClip audioClip)
+            {
+                var oldClipPath = AssetDatabase.GetAssetPath(audioClip);
+                return !oldClipPath.Contains(".wav");
+            }
+
+            return false;
+        }
+
+        [MenuItem("CONTEXT/AudioClip/Convert to wav...", false)]
+        public static void ConvertToWav(MenuCommand menuCommand)
+        {
+            var audioClip = menuCommand.context as AudioClip;
+            var oldClipPath = AssetDatabase.GetAssetPath(audioClip);
+            var newClipPath = oldClipPath.Replace(Path.GetExtension(oldClipPath), ".wav");
+
+            if (File.Exists(newClipPath) &&
+                !EditorUtility.DisplayDialog("Attention!", "Do you want to overwrite the exiting file?", "Yes", "No"))
+            {
+                return;
+            }
+
+            EditorUtility.DisplayProgressBar("Converting clip", $"{oldClipPath} -> {newClipPath}", -1);
+
+            try
+            {
+                File.WriteAllBytes(newClipPath, audioClip.EncodeToWav());
+                AssetDatabase.ImportAsset(newClipPath);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+}
---
+++ Utilities.Encoder.Wav/Packages/com.utilities.encoder.wav/Runtime/WavRecordingBehaviour.cs
@@ -0,0 +1,10 @@
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Utilities.Audio;
+
+namespace Utilities.Encoding.Wav
+{
+    public sealed class WavRecordingBehaviour : AbstractRecordingBehaviour<WavEncoder>
+    {
+    }
+}
